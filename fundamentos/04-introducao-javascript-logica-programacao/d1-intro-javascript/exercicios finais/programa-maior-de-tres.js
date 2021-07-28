29dc7741cece0579bf6eb98ae01cad68266158cb
Lucas <theluks21@live.com>
2021-07-28 18:52:28 -0300

    adiciona programa de retornar maior valor de 3 variaveis

---
+++ fundamentos/04-introducao-javascript-logica-programacao/d1-intro-javascript/exercicios finais/programa-maior-de-tres.js
@@ -0,0 +1,13 @@
+let num1 = 600;
+let num2 = 600;
+let num3 = 600;
+
+if (num1 > num2) {
+    console.log(num1);
+} else if (num2 > num3) {
+    console.log(num2)
+} else if (num3 > num1) {
+    console.log(num3)
+} else {
+    console.log("valor não aceito, tente novamente")
+}
