c8764f441c9043a8359afe3ebb88d7287f8cb5b3
Lucas <theluks21@live.com>
2021-07-28 18:20:03 -0300

    adiciona novo exercicio de estrutura condicional com switch/case

---
+++ fundamentos/04-introducao-javascript-logica-programacao/d1-intro-javascript/script2.js
@@ -0,0 +1,18 @@
+let candidata;
+candidata = "lista";
+
+
+switch (candidata) {
+    case "aprovada":
+        console.log("Parabéns, você foi aprovada(o)!");
+        break;
+    
+    case "lista":
+        console.log("Você está na nossa lista de espera");
+        break;
+
+    case "reprovada":
+        console.log("Você foi reprovada(o)");
+        break;
+}
+
