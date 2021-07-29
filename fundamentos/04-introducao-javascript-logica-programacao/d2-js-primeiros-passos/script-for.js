72aaf6d26c3e3cb801683c2853bf875b2baf76bf
Lucas <theluks21@live.com>
2021-07-29 18:09:14 -0300

    resolve exercicio sobre o laço for/of

--- fundamentos/04-introducao-javascript-logica-programacao/d2-js-primeiros-passos/script-for.js
+++ fundamentos/04-introducao-javascript-logica-programacao/d2-js-primeiros-passos/script-for.js
@@ -1,5 +1,5 @@
 let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 
-for (let index = 0; index < groceryList.length; index += 1) {
-    console.log(groceryList[index])
+for (let items of groceryList) {
+    console.log(items)
 }
