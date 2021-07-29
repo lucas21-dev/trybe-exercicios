e03af8095a3279be05a014f48b258299a7286ccc
Lucas <theluks21@live.com>
2021-07-29 19:36:38 -0300

    resolve alguns exercicios do dia

---
+++ fundamentos/04-introducao-javascript-logica-programacao/d2-js-primeiros-passos/exercicios-do-dia/script-exe-2.js
@@ -0,0 +1,8 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+let soma = 0;
+for (let lista of numbers) {
+    soma += lista
+}
+console.log(soma)
---
+++ fundamentos/04-introducao-javascript-logica-programacao/d2-js-primeiros-passos/exercicios-do-dia/script-exe-5.js
@@ -0,0 +1,15 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Utilizando for , descubra qual o maior valor contido no array e imprima-o
+let maiorNumero = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    let numeroAtual = numbers[index]
+        if (numeroAtual > maiorNumero) {
+            maiorNumero = numeroAtual
+        } else {
+
+        }
+    }
+    
+console.log(maiorNumero)
