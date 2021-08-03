d8f722fb52c4631fc0d5964001125ddd8d96c63b
Lucas <theluks21@live.com>
2021-08-02 21:38:17 -0300

    adiciona e resolve exercicios 3, 4 e 5

--- fundamentos/04-introducao-javascript-logica-programacao/d4-objetos-e-funcoes/objetos3.js
+++ fundamentos/04-introducao-javascript-logica-programacao/d4-objetos-e-funcoes/objetos3.js
@@ -4,4 +4,10 @@ let info = {
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
+//Faça um for/in que mostre todas as chaves do objeto.
 
+info['recorrente'] = 'Sim'
+
+for (let index in info) {
+    console.log(index)
+}
