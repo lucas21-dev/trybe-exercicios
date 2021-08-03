e68ae6f6834dbd63e73c59e9b9a7cf5c50759e05
Lucas <theluks21@live.com>
2021-08-02 21:00:58 -0300

    refatora o exercicio 1

--- fundamentos/04-introducao-javascript-logica-programacao/d3-logica-de-programacao-e-algoritmos/exercicio1.js
+++ fundamentos/04-introducao-javascript-logica-programacao/d3-logica-de-programacao-e-algoritmos/exercicio1.js
@@ -1,6 +1,6 @@
 //Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n.
 
-let n = 20;
+let n = 10;
 let asteriscos = '*';
 let asteriscos2 = asteriscos.repeat(n)
 if (n > 1) {
