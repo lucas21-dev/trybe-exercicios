551241b8298749357cb1ee49d37303116b639d42
Lucas Rodrigues <theluks21@live.com>
2021-08-27 19:02:42 -0300

    Resolve exercicio usando o metodo sort e template literals

--- fundamentos/07-JS-ES6-testes-unitarios/d1-JS-ES6-let-const-arrowFunctions-template-literals/script.js
+++ fundamentos/07-JS-ES6-testes-unitarios/d1-JS-ES6-let-const-arrowFunctions-template-literals/script.js
@@ -15,4 +15,6 @@ const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 // Seu código aqui.
 
-console.log(oddsAndEvens); // será necessário alterar essa linha 😉
+const sortedOddsAndEvens = oddsAndEvens.sort((a,b) => a - b);
+
+console.log(`Os números ${sortedOddsAndEvens} se encontram ordenados de forma crescente!`); 
