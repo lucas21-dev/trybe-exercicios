ddafe4997707346b54a432c2c23e6d5554383eed
Lucas Rodrigues <theluks21@live.com>
2021-08-31 21:22:33 -0300

    Adiciona o código real do terceiro exercicio

--- fundamentos/07-JS-ES6-testes-unitarios/d3-JS-testes-unitarios/script3.js
+++ fundamentos/07-JS-ES6-testes-unitarios/d3-JS-testes-unitarios/script3.js
@@ -1,13 +1,16 @@
 const assert = require('assert');
 
-function myRemove(arr, item) {
-  let newArr = [];
-  for (let index = 0; index < arr.length; index += 1) {
-    if (item !== arr[index]) {
-      newArr.push(arr[index]);
+function myRemoveWithoutCopy(arr, item) {
+  for (let index = 0, len = arr.length; index < len; index += 1) {
+    if (arr[index] === item) {
+      arr.splice(index, 1);
+      index -= 1;
+      len -= 1;
     }
   }
-  return newArr;
+
+  return arr;
 }
 
 // implemente seus testes aqui
+
