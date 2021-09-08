7a400e5ba595836d2d81db652ec0bfce412c662c
Lucas Rodrigues <theluks21@live.com>
2021-09-08 20:42:43 -0300

    Resolve exercicio 1

--- fundamentos/08-higher-order-functions-js/d3-HOFs-map-filter/exercise1.js
+++ fundamentos/08-higher-order-functions-js/d3-HOFs-map-filter/exercise1.js
@@ -64,3 +64,20 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+
+const expectedResult = [
+  'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+  'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+  'Fundação - Ficção Científica - Isaac Asimov',
+  'Duna - Ficção Científica - Frank Herbert',
+  'A Coisa - Terror - Stephen King',
+  'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+];
+
+function formatedBookNames() {
+  // escreva seu código aqui
+  const bookNames = books.map((id) => `${id.name} - ${id.genre} - ${id.author.name}`);
+  return bookNames;
+}
+
+assert.deepStrictEqual(formatedBookNames(), expectedResult);
