48ecba758d84ae16563185fa3628d611283e5e4c
Lucas Rodrigues <theluks21@live.com>
2021-09-09 19:27:30 -0300

    Resolve o exercicio 1

--- fundamentos/08-higher-order-functions-js/d4-HOFs-reduce/exercise1.js
+++ fundamentos/08-higher-order-functions-js/d4-HOFs-reduce/exercise1.js
@@ -6,9 +6,13 @@ const arrays = [
   [4, 5, 6],
 ];
 
+
 function flatten() {
   // escreva seu código aqui
-  
+  return arrays.reduce((acc, array) => {
+    array.forEach((element) => acc.push(element));
+    return acc;
+  }, []);
 }
 
 assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
