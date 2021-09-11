67244a34c391da2a14aa44dd11e809ca048913ac
Lucas Rodrigues <theluks21@live.com>
2021-09-10 21:14:14 -0300

    Resolve o exercicio 6

--- fundamentos/08-higher-order-functions-js/d5-HOFs-spread-rest-desestructuring-shorthand-default/exercise6.js
+++ fundamentos/08-higher-order-functions-js/d5-HOFs-spread-rest-desestructuring-shorthand-default/exercise6.js
@@ -6,6 +6,9 @@ const chiron = ['Chiron', 'Bugatti', 2016];
 
 // escreva toObject abaixo
 
+const toObject = (array) => ({ name: array[0], brand: array[1], year: array[2] });
+
 assert.deepStrictEqual(toObject(palio), { name: 'Palio', brand: 'Fiat', year: 2019 });
 assert.deepStrictEqual(toObject(shelbyCobra), { name: 'Shelby Cobra', brand: 'Ford', year: 1963 });
 assert.deepStrictEqual(toObject(chiron), { name: 'Chiron', brand: 'Bugatti', year: 2016 });
+
