4f4474c837590683989f9be66d5f6c045064ba45
Lucas Rodrigues <theluks21@live.com>
2021-09-16 20:19:44 -0300

    Resolve primeiro exercicio

--- fundamentos/09-js-assincrono-promises/d2-JS-promises/apiScript.js
+++ fundamentos/09-js-assincrono-promises/d2-JS-promises/apiScript.js
@@ -3,6 +3,19 @@ const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
   // Adicionar lógica aqui!
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+
+  fetch(API_URL, myObject)
+    .then((response) => response.json())
+    .then((response) => {
+      const h2 = document.getElementById('jokeContainer');
+      const p = document.createElement('p');
+      p.innerText = response.joke;
+      h2.appendChild(p);
+    })
 };
 
 window.onload = () => fetchJoke();
