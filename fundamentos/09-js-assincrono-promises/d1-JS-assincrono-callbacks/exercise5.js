1e5b363194e8c7eaed4d4099eaa3601fa68fb808
Lucas Rodrigues <theluks21@live.com>
2021-09-15 21:12:14 -0300

    Resolve o exercicio 5

--- fundamentos/09-js-assincrono-promises/d1-JS-assincrono-callbacks/exercise5.js
+++ fundamentos/09-js-assincrono-promises/d1-JS-assincrono-callbacks/exercise5.js
@@ -15,6 +15,10 @@ const greet = (temperature) =>
 
 // definição da função sendMarsTemperature...
 
+const sendMarsTemperature = (callback) => {
+  const marsTemp = getMarsTemperature();
+  return setTimeout(() => callback(marsTemp), messageDelay())
+}
+
 sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
 sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
-
