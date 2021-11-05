f0892d591b47f9e4ff68ac17a9b31a8d620e2dcf
Lucas Rodrigues <theluks21@live.com>
2021-11-04 21:34:29 -0300

    Adiciona os testes pedidos pelo exercicio

--- Front-End/14-react-testing-library/d3-testando-react-router/exercicio-fix/src/App.test.js
+++ Front-End/14-react-testing-library/d3-testando-react-router/exercicio-fix/src/App.test.js
@@ -1,7 +1,30 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import userEvent from '@testing-library/user-event';
 import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
+describe('Testes do exercicio', () => {
+  test('Testa se o texto de tela incial é renderizado', () => {
+    renderWithRouter(<App />);
   
-});
+    const homeEl = screen.getByRole('heading', {
+      name: 'Você está na página Início',
+    });
+  
+    expect(homeEl).toBeInTheDocument();
+    
+  });
+
+  test('Testa se renderiza o componente Sobre', () => {
+    const { history } = renderWithRouter(<App />);
+    
+    const aboutLink = screen.getByRole('link', { name: 'Sobre' });
+    expect(aboutLink).toBeInTheDocument();
+    
+    userEvent.click(aboutLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+
+    const aboutTitle = screen.getByRole('heading', { name: 'Você está na página Sobre' });
+    expect(aboutTitle).toBeInTheDocument();
+  })
+})
